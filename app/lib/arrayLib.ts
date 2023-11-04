8f04270f92b8c7ea6e6eb3b2c0b0a1a008cc45b1
Joonwan Kim <joonwk@gmail.com>
2023-11-04 13:01:02 +0900

    thumbnail image will be display from cloudinary secure_url

---
+++ app/lib/arrayLib.ts
@@ -0,0 +1,23 @@
+import { Knowhow } from "@prisma/client";
+
+export const getImgUrls = (knowhow: any) => {
+    const imgUrls = knowhow?.knowhowDetailInfo?.knowhowDetailOnCloudinaries?.map((s: any) => {
+        if (s.cloudinaryData.format !== 'pdf') {
+            return s.cloudinaryData.secure_url;
+        };
+    }).flatMap((f: any) => f ? [f] : []);
+    return imgUrls;
+};
+export const getPdfUrls = (knowhow: any) => {
+    const pdfUrls = knowhow?.knowhowDetailInfo?.knowhowDetailOnCloudinaries?.map((s: any) => {
+        if (s.cloudinaryData.format === 'pdf') {
+            return s.cloudinaryData.secure_url;
+        };
+    }).flatMap((f: any) => f ? [f] : []);
+    return pdfUrls;
+};
+export const getThumbnailSecureUrl = (knowhow: any) => {
+    if (!knowhow) return;
+    const secure_url = knowhow.cloudinaryData.secure_url;
+    return secure_url;;
+};
