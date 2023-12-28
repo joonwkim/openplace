250b528e037b23fb22b076db3b3f393cb9b4d8e7
Joonwkim <joonwk@gmail.com>
2023-12-28 14:10:26 +0000

    update ecosystem

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -6,15 +6,16 @@ module.exports = {
 
   deploy: {
     production: {
-      key: 'key.pem',
+      key: 'opendesign2023.pem',
       user: 'ubuntu',
-      host: '3.36.91.10',
+      host: '54.180.128.72',
       ref: 'origin/edit-mode',
       repo: 'git@github.com:joonwkim/openplace.git',
-      path: 'home/ubuntu',
+      path: 'home/ubuntu/projects',
       'pre-deploy-local': '',
-      'post-deploy': 'source ~/.nvm/nvm.sh &&  npm install && prisma db push && prisma generate && npm run build && pm2 reload ecosystem.config.js --env production',
-      'pre-setup': ''
+      'post-deploy': 'source ~/.nvm/nvm.sh && npm install && prisma db push && prisma generate && npm run build && pm2 reload ecosystem.config.js --env production',
+      'pre-setup': '',
+      'ssh_options': 'ForwardAgent=yes'
     }
   }
 };
